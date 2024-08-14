47c7effbd25da95ad6bc6341e2a29fe47fe35eae
mvaldes <mvaldes.revilla@gmail.com>
2024-08-14 11:59:20 -0500

    initial setup for fetching issues

--- lua/linear/issues.lua
+++ lua/linear/issues.lua
@@ -1,20 +1,30 @@
 -- Query the Linear API to fetch issues
-local M = {}
 local curl = require("plenary.curl")
 local linearURL = "https://api.linear.app/graphql"
 local linearAPI = vim.fn.getenv("LINEARTOKEN")
-local issuesPayload = '{ "query": "{ issues { nodes { id title } } }" }'
-local issuesResults = {}
+local issuesPayload = '{ "query": "{ issues { nodes { id state {name} title branchName } } }" }'
+local issueList = {}
 
-M.fetchIssues = function()
-	local request = curl.post(linearURL, {
-		headers = {
-			["Content-Type"] = "application/json",
-			["Authorization"] = linearAPI,
-		},
-		body = issuesPayload,
-	})
-	table.insert(issuesResults, request.body)
+local function fetchIssues()
+	if linearAPI ~= nil then
+		local request = curl.post(linearURL, {
+			headers = {
+				["Content-Type"] = "application/json",
+				["Authorization"] = linearAPI,
+			},
+			body = issuesPayload,
+		})
+		-- decode into json and grab the fields
+		local payload = vim.json.decode(request.body)
+		for _, value in ipairs(payload["data"]["issues"]["nodes"]) do
+			local title = value["title"]
+			local status = value["state"]["name"]
+			local branch = value["branchName"]
+			local issue = { branch = branch, title = title, status = status }
+			table.insert(issueList, issue)
+		end
+	end
 end
 
-return M
+fetchIssues()
+print(issueList)
