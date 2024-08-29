6887a9ca0941c5870793df272a6eeec4cd29ec89
mvaldes <mvaldes.revilla@gmail.com>
2024-08-28 23:11:51 -0500

    feat: ui lookin good

--- lua/linear/ui.lua
+++ lua/linear/ui.lua
@@ -1,9 +1,9 @@
 local M = {}
 local Popup = require("nui.popup")
+local Layout = require("nui.layout")
 
 function M.showUI(issueItem, id)
-	local popup = Popup({
-		enter = true,
+	local popup_main = Popup({
 		border = {
 			style = "rounded",
 			text = {
@@ -11,13 +11,42 @@ function M.showUI(issueItem, id)
 				top_align = "center",
 			},
 		},
-		focusable = true,
+		position = "50%",
+		size = "50%",
+		--   {
+		-- 	width = "40%",
+		-- 	height = "30%",
+		-- },
+	})
+
+	local popup_comments = Popup({
+		border = {
+			style = "rounded",
+			text = {
+				top = "Updates",
+				top_align = "center",
+			},
+		},
 		position = "50%",
 		size = {
 			width = "40%",
-			height = "40%",
+			height = "80%",
 		},
 	})
+
+	local layout = Layout(
+		{
+			position = "50%",
+			size = {
+				width = 80,
+				height = "80%",
+			},
+		},
+		Layout.Box({
+			Layout.Box(popup_main, { size = "40%" }),
+			Layout.Box(popup_comments, { size = "60%" }),
+		}, { dir = "row" })
+	)
 
 	local title = issueItem["data"]["issue"]["title"]
 	local description = issueItem["data"]["issue"]["description"]
@@ -25,28 +54,42 @@ function M.showUI(issueItem, id)
 	local assignee = issueItem["data"]["issue"]["assignee"]["name"]
 	local project = issueItem["data"]["issue"]["project"]["name"]
 	local label = issueItem["data"]["issue"]["priorityLabel"]
-	local commentList = issueItem["data"]["issue"]["comments"]["nodes"]
+	local comment_list = issueItem["data"]["issue"]["comments"]["nodes"]
 	local comments = {}
-	for _, value in ipairs(commentList) do
+	for _, value in ipairs(comment_list) do
 		table.insert(
 			comments,
 			{ comment = value["body"], created = value["createdAt"], username = value["user"]["name"] }
 		)
 	end
 
-	local msg = string.format(
-		"Issue ID: %s \n Title: %s \n State: %s \n Description: %s \n Assignee: %s \n Project:%s \n Label:%s \n Comments:%s ",
-		id,
-		title,
-		state,
-		description,
-		assignee,
-		project,
-		label,
-		comments
-	)
-	vim.api.nvim_buf_set_lines(popup.bufnr, -1, -1, false, { msg })
-	popup:mount()
+	local item_info = {
+		"Title: " .. title,
+		"State: " .. state,
+		"Description: " .. description,
+		"Asignee: " .. assignee,
+		"Project: " .. project,
+		"Label: " .. label,
+	}
+	for _, value in ipairs(item_info) do
+		vim.api.nvim_buf_set_lines(popup_main.bufnr, -1, -1, false, { value })
+	end
+	for _, value in ipairs(comments) do
+		local update = vim.split(value["comment"], "\n")
+		vim.api.nvim_buf_set_lines(popup_comments.bufnr, -1, -1, false, { "Username: " .. value["username"] })
+		vim.api.nvim_buf_set_lines(popup_comments.bufnr, -1, -1, false, { "Created: " .. value["created"] })
+		for _, comment in ipairs(update) do
+			vim.api.nvim_buf_set_lines(popup_comments.bufnr, -1, -1, false, { comment })
+		end
+		vim.api.nvim_buf_set_lines(popup_comments.bufnr, -1, -1, false, { "----------------------" })
+	end
+
+	layout:update(Layout.Box({
+		Layout.Box(popup_main, { size = "40%" }),
+		Layout.Box(popup_comments, { size = "60%" }),
+	}, { dir = "col" }))
+
+	layout:mount()
 end
 
 return M
